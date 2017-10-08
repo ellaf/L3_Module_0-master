2f203947b69d99127d4e0ec0f5bf50098ad42844
ellaf <league@WorthyMini-6.attlocal.net>
2017-10-08 11:31:21 -0700

    Finished Intro to Hash Maps! :)

--- src/IntroToHashMaps/IntroToHashMaps.java
+++ src/IntroToHashMaps/IntroToHashMaps.java
@@ -1,10 +1,30 @@
 package IntroToHashMaps;
 
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class IntroToHashMaps {
 	//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
-	
+
 	//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 	//   current iteration of the loop (i). The value entry will be the square root of i.
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
+		
+		for (int i = 0; i < 500; i++) {
+			
+			roots.put(i, Math.sqrt(i));
+			System.out.println("The square root of " + i + " is " + roots.get(i));
+			
+		}
+		
+		
+	}
+
 	
 	//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 	//   square roots (values). Use the following format.
--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,8 +1,18 @@
 package IntroToHashMaps;
 
-public class LogSearch {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class LogSearch implements ActionListener{
   /* 
-	 * Crate a HashMap of Integers for the keys and Strings for the values.
+	 * Crate a HashMap of Integers for the keys and Strings for the values. 
+
 	 * Create a GUI with three buttons. 
 	 * Button 1: Add Entry
 	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
@@ -28,4 +38,97 @@ public class LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	
+	
+	HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton buttonOne = new JButton("Add Entry");
+	JButton buttonTwo = new JButton("Search by ID");
+	JButton buttonThree = new JButton("View List");
+	JButton buttonFour = new JButton("Remove Entry");
+	
+	public static void main(String[] args) {
+		
+	LogSearch logsearch = new LogSearch();
+	logsearch.setup();
+		
+		
+	}
+	
+	void setup(){
+		
+		frame.add(panel);
+		panel.add(buttonOne);
+		panel.add(buttonTwo);
+		panel.add(buttonThree);
+		panel.add(buttonFour);
+		buttonOne.addActionListener(this);
+		buttonTwo.addActionListener(this);
+		buttonThree.addActionListener(this);
+		buttonFour.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if(e.getSource() == buttonOne) {
+			
+			String id = JOptionPane.showInputDialog("Please enter an ID number.");
+			int idInt = Integer.parseInt(id);
+			String name = JOptionPane.showInputDialog("Please enter a name.");
+			hashmap.put(idInt, name);
+			
+		}
+		if(e.getSource() == buttonTwo) {
+			
+		String searchedId = JOptionPane.showInputDialog("Please enter the ID you want to search.");
+		int searchedIdInt = Integer.parseInt(searchedId);
+		if(hashmap.keySet().contains(searchedIdInt)) {
+			
+			JOptionPane.showMessageDialog(null, "That is the ID of: " + hashmap.get(searchedIdInt) + ".");
+			
+			
+		}else{
+			
+			JOptionPane.showMessageDialog(null, "I'm sorry, that ID does not exist.");
+			
+		}
+			
+		}
+		if(e.getSource() == buttonThree) {
+			
+			String idList = "";
+			
+			for (Integer i : hashmap.keySet()) {
+				
+				idList += "ID: " + i + "   Name: " + hashmap.get(i) + "\n";
+				
+			}
+	
+			JOptionPane.showMessageDialog(null, idList);
+			
+			
+		}
+		if(e.getSource() == buttonFour) {
+			
+			String removeEntry = JOptionPane.showInputDialog("Please enter the ID you'd like to remove.");
+			int removeEntryInt = Integer.parseInt(removeEntry);
+			if(hashmap.keySet().contains(removeEntryInt)){
+				
+			hashmap.remove(removeEntryInt);
+			JOptionPane.showMessageDialog(null, "That ID has been removed.");
+				
+			}
+			
+			
+		}
+		
+	}
+
+	
 }
